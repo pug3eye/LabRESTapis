2db27a442ddfd5ba16dee07eb4085b046f915800
pug3eye <pug3eye@gmail.com>
2017-09-13 17:24:31 +0700

    first commit

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import * as logger from 'morgan';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+
+// import routers
+
+
+// Server class
+class Server {
+
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    public config() {
+        // set up mongoose
+        const MONGO_URI = 'mongodb://localhost/LabRESTapis'
+        mongoose.connect(MONGO_URI || process.env.MONGODB_URI)
+        
+        // config
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
+        this.app.use(logger('dev'));
+        this.app.use(compression());
+        this.app.use(helmet());
+        this.app.use(cors());
+
+        // this.app.use((req) => {
+        //     // cors options
+        // })
+    }
+
+    public routes(): void {
+        let router: express.Router;
+        router = express.Router();
+
+        this.app.use('/', router);
+        // this.app.use('/api/v1/posts', PostRouter);
+
+    }
+}
+
+// export
+export default new Server().app;
